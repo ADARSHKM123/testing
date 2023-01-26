3cfa9f2f68ba008bd128fea666b2895cf6c817bd
ADARSH123 <adarshkmeethal@gmail.com>
2023-01-26 15:03:47 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useState } from 'react';
+// import {Router, Route} from 'react-router';
+
 
 function App() {
+
+  const [country, setcountry] = useState('india');
+  const [countryselect, setcountryselect] = useState('');
+  const [CountryDta, setCountryDta] = useState([])
+
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    setcountry(countryselect);
+  }
+
+  const handleChange =(e)=>{
+   console.log(e.target.value);
+   setcountryselect(e.target.value)
+  }
+
+  const collecting=async ()=>{
+   await axios.get(`https://restcountries.com/v2/name/${country}`).then(res=>{
+      console.log(res.data);
+      setCountryDta(res.data)
+    })
+  }
+
+
+
   return (
+  <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Get Your country details:
+          <select  onChange={handleChange}>
+            <option value="india">India</option>
+            <option value="spain">spain</option>
+            <option value="usa">usa</option>
+          </select>
+        </label>
+        <input className='btn' type="submit" />
+      </form>
     </div>
+    <div>
+    <button className='mainbtn' onClick={collecting}>Get all data</button>
+    </div>
+
+<table>
+  <thead>
+    <tr>
+    <th className='th'>Capital</th>
+    <th className='th'>Population</th>
+    <th className='th'>Region</th>
+    </tr>
+  </thead>
+  {
+    CountryDta.map((each,i)=>{
+      return(
+        <tbody key={i}>
+        <td>{each.capital}</td>
+        <td>{each.population}</td>
+        <td>{each.region}</td>
+      </tbody>
+      )
+    })
+  }
+</table>
+    </>
+
   );
 }
 
